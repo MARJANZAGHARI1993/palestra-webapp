9841458b00e71473452fcdfe2a3ea95dc3df1718
marjan <marjan.zaghari1993@yahoo.com>
2025-02-06 10:55:07 +0100

    commincio il progetto

---
+++ palestra-webapp/src/main/java/com/example/palestra_webapp/PalestraWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.palestra_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalestraWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalestraWebappApplication.class, args);
+	}
+
+}
