62f34e45cd3b42f1ec46a8f50c4eeb3c7d18d676
Maurizia95 <mauriziaporcaro@gmail.com>
2025-02-06 16:27:41 +0100

    implementazione classe disciplina

--- palestra-webapp/src/main/java/com/example/palestra_webapp/model/Disciplina.java
+++ palestra-webapp/src/main/java/com/example/palestra_webapp/model/Disciplina.java
@@ -1,4 +1,66 @@
 package com.example.palestra_webapp.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "discipline")
 public class Disciplina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String descrizione;
+
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "fk_id_insegnante", referencedColumnName = "id")
+    private Insegnante insegnante;
+
+    @Column
+    private double prezzoUnitario;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Insegnante getInsegnante() {
+        return insegnante;
+    }
+
+    public void setInsegnante(Insegnante insegnante) {
+        this.insegnante = insegnante;
+    }
+
+    public double getPrezzoUnitario() {
+        return prezzoUnitario;
+    }
+
+    public void setPrezzoUnitario(double prezzoUnitario) {
+        this.prezzoUnitario = prezzoUnitario;
+    }
 }
