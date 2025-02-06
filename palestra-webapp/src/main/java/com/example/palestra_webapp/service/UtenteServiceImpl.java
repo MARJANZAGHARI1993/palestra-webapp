5ed9c870878e0f947f5900a08b531d4eb5a40aae
marjan <marjan.zaghari1993@yahoo.com>
2025-02-06 17:33:22 +0100

    implementare classe utente servic+impl

--- palestra-webapp/src/main/java/com/example/palestra_webapp/service/UtenteServiceImpl.java
+++ palestra-webapp/src/main/java/com/example/palestra_webapp/service/UtenteServiceImpl.java
@@ -1,4 +1,35 @@
 package com.example.palestra_webapp.service;
 
-public class UtenteServiceImpl {
+import com.example.palestra_webapp.dao.UtenteDao;
+import com.example.palestra_webapp.model.Utente;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UtenteServiceImpl implements UtenteService {
+
+    @Autowired
+    private UtenteDao utenteDao;
+
+    @Override
+    public boolean loginUtente(String username, String password,
+                               HttpSession session) {
+        Utente utente = utenteDao.findByProfiloUsernameAndProfiloPassword(username,password);
+        if(utente != null){
+            session.setAttribute("utente",utente);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void registrazioneUtente(Utente utente) {
+        utenteDao.save(utente);
+    }
+
+    @Override
+    public boolean controlloUsername(String username) {
+        return utenteDao.findByProfiloUsername(username) == null;
+    }
 }
