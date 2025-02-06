754d99c24b39fa93043f9c414980ae8e00b9109a
itsggiorgia <itsggiorgia@gmail.com>
2025-02-06 16:22:44 +0100

    compilazione classe incontro

--- palestra-webapp/src/main/java/com/example/palestra_webapp/model/Incontro.java
+++ palestra-webapp/src/main/java/com/example/palestra_webapp/model/Incontro.java
@@ -1,4 +1,41 @@
 package com.example.palestra_webapp.model;
 
+import jakarta.persistence.*;
+
 public class Incontro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(cascade = CascadeType.REFRESH) // molti a uno, un incontro è associato a una sola disciplina, ma una disciplina può essere collegata a più incontri. REFRESH: possiamo aggiornare la disciplina se è cambiata nel database, ma senza eliminarla o modificarla automaticamente.
+    @JoinColumn(name = "fk_id_disciplina", referencedColumnName = "id")
+    private Disciplina disciplina;
+
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "fk_id_insegnante", referencedColumnName = "id")
+    private Insegnante insegnante;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public Insegnante getInsegnante() {
+        return insegnante;
+    }
+
+    public void setInsegnante(Insegnante insegnante) {
+        this.insegnante = insegnante;
+    }
 }
