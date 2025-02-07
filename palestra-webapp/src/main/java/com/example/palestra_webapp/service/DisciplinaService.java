6feaa0a4dc8a6e22630b1558a52af37075a2ddd2
Maurizia95 <mauriziaporcaro@gmail.com>
2025-02-07 11:22:30 +0100

    implementazione disciplina service e impl

--- palestra-webapp/src/main/java/com/example/palestra_webapp/service/DisciplinaService.java
+++ palestra-webapp/src/main/java/com/example/palestra_webapp/service/DisciplinaService.java
@@ -2,10 +2,13 @@ package com.example.palestra_webapp.service;
 
 import com.example.palestra_webapp.model.Disciplina;
 import java.util.List;
+import java.util.Optional;
 
 public interface DisciplinaService {
     List<Disciplina> elencoDiscipline();
     Disciplina datiDisciplina(int id);
+
+    Optional<Disciplina> getDisciplinaById(int id);
 }
 
 
--- palestra-webapp/src/main/java/com/example/palestra_webapp/service/DisciplinaServiceImpl.java
+++ palestra-webapp/src/main/java/com/example/palestra_webapp/service/DisciplinaServiceImpl.java
@@ -1,8 +1,7 @@
 package com.example.palestra_webapp.service;
 
-import com.example.palestra_webapp.dao.DisciplinaDao;  // Importa il DAO (o Repository)
+import com.example.palestra_webapp.dao.DisciplinaDao;
 import com.example.palestra_webapp.model.Disciplina;
-import com.example.palestra_webapp.service.DisciplinaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,20 +14,29 @@ public class DisciplinaServiceImpl implements DisciplinaService {
     @Autowired
     private DisciplinaDao disciplinaDao;
 
-    // Implementazione del metodo per recuperare tutte le discipline
     @Override
     public List<Disciplina> elencoDiscipline() {
         return (List<Disciplina>) disciplinaDao.findAll();
     }
 
-    // Implementazione del metodo per recuperare i dettagli di una disciplina
     @Override
     public Disciplina datiDisciplina(int id) {
-        // Trova la disciplina per ID e restituisce un oggetto Optional
-        Optional<Disciplina> disciplinaOptional = disciplinaDao.findById(id);
-        // Restituisce la disciplina se trovata, altrimenti restituisce null
-        return disciplinaOptional.orElse(null);
+        return disciplinaDao.findById(id).get();
     }
 
+    @Override
+    public Optional<Disciplina> getDisciplinaById(int id) {
+        try {
+            Optional<Disciplina> disciplinaOptional = disciplinaDao.findById(id);
+            if (disciplinaOptional.isPresent()) {
+                return Optional.of(disciplinaOptional.get());
+            } else {
+                System.out.println("Nessuna disciplina trovata con l'ID: " + id);
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            System.out.println("Errore nella ricerca della disciplina: " + e.getMessage());
+            return Optional.empty();
+        }
+    }
 }
-
