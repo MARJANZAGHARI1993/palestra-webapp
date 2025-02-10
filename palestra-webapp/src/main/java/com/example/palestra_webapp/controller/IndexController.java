de09a4fb1ff659334ea2b42269c9616fdbfd0ee3
Bianca-Maria96 <biancamariadefocatiis@gmail.com>
2025-02-10 11:50:03 +0100

    commit index

--- palestra-webapp/src/main/java/com/example/palestra_webapp/controller/IndexController.java
+++ palestra-webapp/src/main/java/com/example/palestra_webapp/controller/IndexController.java
@@ -1,15 +1,11 @@
 package com.example.palestra_webapp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
-
-// localhost:8080 (sostituito da libreria.it)
+// localhost:8080
 @Controller
 @RequestMapping("/")
 public class IndexController {
