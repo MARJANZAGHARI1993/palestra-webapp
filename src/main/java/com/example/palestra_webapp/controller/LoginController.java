683318ba0bc12b06c225cbca3cfc7b9e39c68915
adrianapollio <adrianapollio@icloud.com>
2025-02-11 10:58:13 +0100

    modifiche login

--- src/main/java/com/example/palestra_webapp/controller/LoginController.java
+++ src/main/java/com/example/palestra_webapp/controller/LoginController.java
@@ -5,12 +5,13 @@ import com.example.palestra_webapp.service.UtenteService;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 // localhost:8080/login
-@RestController
+@Controller
 @RequestMapping("/login")
 public class LoginController {
 
