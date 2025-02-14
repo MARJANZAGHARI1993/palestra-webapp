20ec568d62dd81fefcf4c7218a46b407479a80fc
Maurizia95 <mauriziaporcaro@gmail.com>
2025-02-14 18:04:36 +0100

    insegnanti service impl + foto

--- src/main/java/com/example/palestra_webapp/service/InsegnanteServiceImpl.java
+++ src/main/java/com/example/palestra_webapp/service/InsegnanteServiceImpl.java
@@ -21,24 +21,32 @@ public class InsegnanteServiceImpl implements InsegnanteService {
         for (Insegnante insegnante : insegnanti) {
             if (insegnante.getNome().equals("Ginevra") && insegnante.getCognome().equals("Ruggeri")) {
                 insegnante.setDescrizione("6 anni di esperienza in Pilates. Specializzata in...");
-                insegnante.setFotoUrl("/img/ins2Pilates.jpg");
+                insegnante.setFotoUrl("/img/ginevra.jpg");
                 insegnante.setDisciplina("pilates");// Percorso relativo alla cartella static
             } else if (insegnante.getNome().equals("Marco") && insegnante.getCognome().equals("Verdi")) {
                 insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo...");
                 insegnante.setFotoUrl("/img/instaichi.jpg");
+                insegnante.setDisciplina("pilates");// Percorso relativo alla cartella static
+            } else if (insegnante.getNome().equals("Beatrice") && insegnante.getCognome().equals("Verdi")) {
+                insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo...");
+                insegnante.setFotoUrl("/img/beatrice.jpg");
+                insegnante.setDisciplina("yoga");// Percorso relativo alla cartella static
+            } else if (insegnante.getNome().equals("Sara") && insegnante.getCognome().equals("Gialli")) {
+                insegnante.setDescrizione("10 anni di esperienza. Approccio innovativo...");
+                insegnante.setFotoUrl("/img/sara.png");
                 insegnante.setDisciplina("taichi");// Percorso relativo alla cartella static
             } else if (insegnante.getNome().equals("Elian") && insegnante.getCognome().equals("Varnesi")) {
                 insegnante.setDescrizione("15 anni di esperienza in Tai Chi. Esperto in...");
-                insegnante.setFotoUrl("/img/ins1Q.jpg");
+                insegnante.setFotoUrl("/img/elian.jpg");
                 insegnante.setDisciplina("qigong");// Percorso relativo alla cartella static
-            } else if (insegnante.getNome().equals("Anna") && insegnante.getCognome().equals("Rossi")) {
-                insegnante.setDescrizione("Istruttrice di Yoga.");
-                insegnante.setFotoUrl("/img/insYoga1.jpg");
-                insegnante.setDisciplina("yoga");
-            } else if (insegnante.getNome().equals("Luca") && insegnante.getCognome().equals("Bianchi")) {
-                insegnante.setDescrizione("Personal Trainer");
-                insegnante.setFotoUrl("/img/insPilates.jpg");
-                insegnante.setDisciplina("pilates");
+            } else if (insegnante.getNome().equals("Elisa") && insegnante.getCognome().equals("Caruso")) {
+                insegnante.setDescrizione("Istruttrice di Tai-Chi.");
+                insegnante.setFotoUrl("/img/elisa.jpg");
+                insegnante.setDisciplina("taichi");
+            } else if (insegnante.getNome().equals("Taren") && insegnante.getCognome().equals("Dravik")) {
+                insegnante.setDescrizione("Insegnante Qigong");
+                insegnante.setFotoUrl("/img/taren1.png");
+                insegnante.setDisciplina("qigong");
             } else {
                 insegnante.setDescrizione("Descrizione predefinita per " + insegnante.getNome() + " " + insegnante.getCognome());
                 insegnante.setFotoUrl("/img/default.jpg");
@@ -56,30 +64,39 @@ public class InsegnanteServiceImpl implements InsegnanteService {
         // Imposta manualmente la descrizione e la foto per il singolo insegnante
         if (insegnante.getNome().equals("Ginevra") && insegnante.getCognome().equals("Ruggeri")) {
             insegnante.setDescrizione("6 anni di esperienza in Pilates. Specializzata in...");
-            insegnante.setFotoUrl("/img/ins2Pilates.jpg");
-            insegnante.setDisciplina("pilates");// Percorso relativo alla cartella static
-        }  else if (insegnante.getNome().equals("Marco") && insegnante.getCognome().equals("Verdi")) {
+            insegnante.setFotoUrl("/img/ginevra.jpg");
+            insegnante.setDisciplina("pilates");
+        } else if (insegnante.getNome().equals("Marco") && insegnante.getCognome().equals("Verdi")) {
             insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo...");
             insegnante.setFotoUrl("/img/instaichi.jpg");
+            insegnante.setDisciplina("pilates");
+        } else if (insegnante.getNome().equals("Beatrice") && insegnante.getCognome().equals("Verdi")) {
+            insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo...");
+            insegnante.setFotoUrl("/img/beatrice.jpg");
+            insegnante.setDisciplina("yoga");
+        } else if (insegnante.getNome().equals("Sara") && insegnante.getCognome().equals("Gialli")) {
+            insegnante.setDescrizione("10 anni di esperienza. Approccio innovativo...");
+            insegnante.setFotoUrl("/img/sara.png");
             insegnante.setDisciplina("taichi");
         } else if (insegnante.getNome().equals("Elian") && insegnante.getCognome().equals("Varnesi")) {
             insegnante.setDescrizione("15 anni di esperienza in Tai Chi. Esperto in...");
-            insegnante.setFotoUrl("/img/ins1Q.jpg");
+            insegnante.setFotoUrl("/img/elian.jpg");
             insegnante.setDisciplina("qigong");
-        } else if (insegnante.getNome().equals("Anna") && insegnante.getCognome().equals("Rossi")) {
-            insegnante.setDescrizione("Istruttrice di Yoga.");
-            insegnante.setFotoUrl("/img/insYoga1.jpg");
-            insegnante.setDisciplina("yoga");
-        } else if (insegnante.getNome().equals("Luca") && insegnante.getCognome().equals("Bianchi")) {
-            insegnante.setDescrizione("Personal Trainer");
-            insegnante.setFotoUrl("/img/insPilates.jpg");
-            insegnante.setDisciplina("pilates");
+        } else if (insegnante.getNome().equals("Elisa") && insegnante.getCognome().equals("Caruso")) {
+            insegnante.setDescrizione("Istruttrice di Tai-Chi.");
+            insegnante.setFotoUrl("/img/elisa.jpg");
+            insegnante.setDisciplina("taichi");
+        } else if (insegnante.getNome().equals("Taren") && insegnante.getCognome().equals("Dravik")) {
+            insegnante.setDescrizione("Insegnante Qigong");
+            insegnante.setFotoUrl("/img/taren1.png");
+            insegnante.setDisciplina("qigong");
         } else {
             insegnante.setDescrizione("Descrizione predefinita per " + insegnante.getNome() + " " + insegnante.getCognome());
             insegnante.setFotoUrl("/img/default.jpg");
-            insegnante.setDisciplina("default");// Assicurati di avere un'immagine predefinita
+            insegnante.setDisciplina("default"); // Assicurati di avere un'immagine predefinita
         }
 
         return insegnante;
     }
 }
+
