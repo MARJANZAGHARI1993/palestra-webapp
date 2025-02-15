ec37e4d3335506ce9569fa7c5340cc70ced5aaa3
Maurizia95 <mauriziaporcaro@gmail.com>
2025-02-15 17:17:24 +0100

    descrizioni

--- src/main/java/com/example/palestra_webapp/service/InsegnanteServiceImpl.java
+++ src/main/java/com/example/palestra_webapp/service/InsegnanteServiceImpl.java
@@ -20,39 +20,32 @@ public class InsegnanteServiceImpl implements InsegnanteService {
         // Imposta manualmente le descrizioni e le foto
         for (Insegnante insegnante : insegnanti) {
             if (insegnante.getNome().equals("Ginevra") && insegnante.getCognome().equals("Ruggeri")) {
-                insegnante.setDescrizione("6 anni di esperienza in Pilates. Specializzata in...");
+                insegnante.setDescrizione("6 anni di esperienza in Pilates. Specializzata nel miglioramento della postura e nel rafforzamento muscolare, Ginevra utilizza tecniche avanzate per adattare ogni lezione alle esigenze individuali degli studenti.");
                 insegnante.setFotoUrl("/img/ginevra.jpg");
-                insegnante.setDisciplina("pilates");// Percorso relativo alla cartella static
             } else if (insegnante.getNome().equals("Marco") && insegnante.getCognome().equals("Verdi")) {
-                insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo...");
+                insegnante.setDescrizione("Marco Verdi, con 12 anni di esperienza nel Pilates, utilizza attrezzi come il reformer e il Cadillac per migliorare forza, flessibilità e postura con programmi personalizzati.");
                 insegnante.setFotoUrl("/img/instaichi.jpg");
-                insegnante.setDisciplina("pilates");// Percorso relativo alla cartella static
             } else if (insegnante.getNome().equals("Beatrice") && insegnante.getCognome().equals("Verdi")) {
-                insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo...");
+                insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo che combina tecniche tradizionali e moderne per migliorare forza, flessibilità e consapevolezza corporea. Marco personalizza ogni lezione per ottimizzare i risultati individuali.");
                 insegnante.setFotoUrl("/img/beatrice.jpg");
-                insegnante.setDisciplina("yoga");// Percorso relativo alla cartella static
             } else if (insegnante.getNome().equals("Sara") && insegnante.getCognome().equals("Gialli")) {
-                insegnante.setDescrizione("10 anni di esperienza. Approccio innovativo...");
+                insegnante.setDescrizione("10 anni di esperienza nell'insegnamento dello yoga. Approccio innovativo che integra tecniche di rilassamento e potenziamento per promuovere equilibrio, flessibilità e benessere mentale.");
                 insegnante.setFotoUrl("/img/sara.png");
-                insegnante.setDisciplina("taichi");// Percorso relativo alla cartella static
             } else if (insegnante.getNome().equals("Elian") && insegnante.getCognome().equals("Varnesi")) {
-                insegnante.setDescrizione("15 anni di esperienza in Tai Chi. Esperto in...");
+                insegnante.setDescrizione("15 anni di esperienza nel Tai Chi. Esperto nella combinazione di movimenti fluidi e respirazione per migliorare l'equilibrio, la concentrazione e il benessere fisico.");
                 insegnante.setFotoUrl("/img/elian.jpg");
-                insegnante.setDisciplina("qigong");// Percorso relativo alla cartella static
             } else if (insegnante.getNome().equals("Elisa") && insegnante.getCognome().equals("Caruso")) {
-                insegnante.setDescrizione("Istruttrice di Tai-Chi.");
+                insegnante.setDescrizione("Istruttrice di Tai Chi con un approccio olistico, mirato a migliorare la postura, la mobilità articolare e la calma interiore.");
                 insegnante.setFotoUrl("/img/elisa.jpg");
                 insegnante.setDisciplina("taichi");
             } else if (insegnante.getNome().equals("Taren") && insegnante.getCognome().equals("Dravik")) {
-                insegnante.setDescrizione("Insegnante Qigong");
+                insegnante.setDescrizione("Insegnante di Qigong, specializzato nella pratica dell'energia vitale per il miglioramento del benessere fisico e mentale.");
                 insegnante.setFotoUrl("/img/taren1.png");
-                insegnante.setDisciplina("qigong");
             } else {
                 insegnante.setDescrizione("Descrizione predefinita per " + insegnante.getNome() + " " + insegnante.getCognome());
-                insegnante.setFotoUrl("/img/default.jpg");
-                insegnante.setDisciplina("default"); // Assicurati di avere un'immagine predefinita
-            }
+                insegnante.setFotoUrl("/img/default.jpg");}
         }
+
 
         return insegnanti;
     }
@@ -63,37 +56,29 @@ public class InsegnanteServiceImpl implements InsegnanteService {
 
         // Imposta manualmente la descrizione e la foto per il singolo insegnante
         if (insegnante.getNome().equals("Ginevra") && insegnante.getCognome().equals("Ruggeri")) {
-            insegnante.setDescrizione("6 anni di esperienza in Pilates. Specializzata in...");
+            insegnante.setDescrizione("6 anni di esperienza in Pilates. Specializzata nel miglioramento della postura e nel rafforzamento muscolare, Ginevra utilizza tecniche avanzate per adattare ogni lezione alle esigenze individuali degli studenti.");
             insegnante.setFotoUrl("/img/ginevra.jpg");
-            insegnante.setDisciplina("pilates");
         } else if (insegnante.getNome().equals("Marco") && insegnante.getCognome().equals("Verdi")) {
-            insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo...");
+            insegnante.setDescrizione("Marco Verdi, con 12 anni di esperienza nel Pilates, utilizza attrezzi come il reformer e il Cadillac per migliorare forza, flessibilità e postura con programmi personalizzati.");
             insegnante.setFotoUrl("/img/instaichi.jpg");
-            insegnante.setDisciplina("pilates");
         } else if (insegnante.getNome().equals("Beatrice") && insegnante.getCognome().equals("Verdi")) {
-            insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo...");
+            insegnante.setDescrizione("12 anni di esperienza. Approccio innovativo che combina tecniche tradizionali e moderne per migliorare forza, flessibilità e consapevolezza corporea. Marco personalizza ogni lezione per ottimizzare i risultati individuali.");
             insegnante.setFotoUrl("/img/beatrice.jpg");
-            insegnante.setDisciplina("yoga");
         } else if (insegnante.getNome().equals("Sara") && insegnante.getCognome().equals("Gialli")) {
-            insegnante.setDescrizione("10 anni di esperienza. Approccio innovativo...");
+            insegnante.setDescrizione("10 anni di esperienza nell'insegnamento dello yoga. Approccio innovativo che integra tecniche di rilassamento e potenziamento per promuovere equilibrio, flessibilità e benessere mentale.");
             insegnante.setFotoUrl("/img/sara.png");
-            insegnante.setDisciplina("taichi");
         } else if (insegnante.getNome().equals("Elian") && insegnante.getCognome().equals("Varnesi")) {
-            insegnante.setDescrizione("15 anni di esperienza in Tai Chi. Esperto in...");
+            insegnante.setDescrizione("15 anni di esperienza nel Tai Chi. Esperto nella combinazione di movimenti fluidi e respirazione per migliorare l'equilibrio, la concentrazione e il benessere fisico.");
             insegnante.setFotoUrl("/img/elian.jpg");
-            insegnante.setDisciplina("qigong");
         } else if (insegnante.getNome().equals("Elisa") && insegnante.getCognome().equals("Caruso")) {
-            insegnante.setDescrizione("Istruttrice di Tai-Chi.");
+            insegnante.setDescrizione("Istruttrice di Tai Chi con un approccio olistico, mirato a migliorare la postura, la mobilità articolare e la calma interiore.");
             insegnante.setFotoUrl("/img/elisa.jpg");
-            insegnante.setDisciplina("taichi");
         } else if (insegnante.getNome().equals("Taren") && insegnante.getCognome().equals("Dravik")) {
-            insegnante.setDescrizione("Insegnante Qigong");
+            insegnante.setDescrizione("Insegnante di Qigong, specializzato nella pratica dell'energia vitale per il miglioramento del benessere fisico e mentale.");
             insegnante.setFotoUrl("/img/taren1.png");
-            insegnante.setDisciplina("qigong");
         } else {
             insegnante.setDescrizione("Descrizione predefinita per " + insegnante.getNome() + " " + insegnante.getCognome());
             insegnante.setFotoUrl("/img/default.jpg");
-            insegnante.setDisciplina("default"); // Assicurati di avere un'immagine predefinita
         }
 
         return insegnante;
